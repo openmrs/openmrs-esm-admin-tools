c2021f9d9c7e1682212ce41a9ffca4dddf59de64
Piumal Rathnayake <piumalrathnayake@hotmail.com>
2022-07-14 18:05:56 +0530

    O3-1336: Create component for importing concepts (#3)

--- packages/esm-admin-openconceptlab-app/src/types.ts
+++ packages/esm-admin-openconceptlab-app/src/types.ts
@@ -5,3 +5,20 @@ export interface Subscription {
   subscribedToSnapshot?: boolean;
   validationType?: 'NONE' | 'FULL';
 }
+
+export interface Import {
+  uuid: string;
+  localDateStarted: Date;
+  localDateStopped: Date;
+  importTime: string;
+  errorMessage: string;
+  importProgress: number;
+  allItemsCount: number;
+  errorItemsCount: number;
+  ignoredErrorsCount: number;
+  updatedItemsCount: number;
+  upToDateItemsCount: number;
+  retiredItemsCount: number;
+  unretiredItemsCount: number;
+  status: string;
+}
