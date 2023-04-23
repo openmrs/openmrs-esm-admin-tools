e8cad874fbc1d6ec2c3a440c2992b4d3585ce1da
piumal1999 <piumalrathnayake@hotmail.com>
2023-04-24 01:40:08 +0530

    O3-2070: Create e2e test for importing using subscription

---
+++ e2e/commands/oclOperations.ts
@@ -0,0 +1,20 @@
+import { APIRequestContext, expect } from '@playwright/test';
+
+export interface Subscription {
+  uuid: string;
+  url: string;
+  token: string;
+  subscribedToSnapshot?: boolean;
+  validationType?: 'NONE' | 'FULL';
+}
+
+export const getSavedSubscription = async (api: APIRequestContext): Promise<Subscription> => {
+  const subscriptionRes = await api.get('openconceptlab/subscription?v=full');
+  await expect(subscriptionRes.ok()).toBeTruthy();
+  const { results } = await subscriptionRes.json();
+  return results[0];
+};
+
+export const removeOclSubscription = async (api: APIRequestContext, subscription: Subscription) => {
+  await api.delete(`openconceptlab/subscription/${subscription.uuid}`);
+};
--- e2e/pages/openConceptLabPage.ts
+++ e2e/pages/openConceptLabPage.ts
@@ -3,7 +3,26 @@ import { Page } from '@playwright/test';
 export class OpenConceptLabPage {
   constructor(readonly page: Page) {}
 
+  readonly subscriptionTab = () => this.page.getByRole('tab', { name: 'Subscription' });
+  readonly importTab = () => this.page.getByRole('tab', { name: 'Import', exact: true });
+  readonly previousImportsTab = () => this.page.getByRole('tab', { name: 'Previous Imports' });
+  readonly previousImportsTable = () => this.page.getByRole('table');
+
   async goto() {
     await this.page.goto('ocl');
+  }
+
+  async addOclSubscription() {
+    await this.page.getByLabel('Subscription URL').fill(process.env.E2E_OCL_SUBSCRIPTION_URL);
+    await this.page.getByLabel('Token').fill(process.env.E2E_OCL_TOKEN);
+    await this.page.getByRole('button', { name: 'Save Changes' }).click();
+  }
+
+  async startImport() {
+    await this.page.getByRole('button', { name: 'Import from Subscription' }).click();
+  }
+
+  async unsubscribe() {
+    await this.page.getByRole('button', { name: 'Unsubscribe' }).click();
   }
 }
