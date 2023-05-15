cae1aba3cc17887eca0b02855dc6c3b20a1886d0
Ian <ian_bacher@brown.edu>
2023-05-15 10:57:49 -0400

    (chore) Update tooling to Yarn v3 etc.

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  transform: {
+    '^.+\\.(m?j|t)sx?$': ['@swc/jest'],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@openmrs|dexie)'],
+  moduleNameMapper: {
+    '\\.(s?css)$': 'identity-obj-proxy',
+    '@openmrs/esm-framework': '@openmrs/esm-framework/mock',
+    '^lodash-es/(.*)$': 'lodash/$1',
+  },
+  collectCoverageFrom: [
+    '**/src/**/*.component.tsx',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/src/**/*.test.*',
+    '!**/src/declarations.d.tsx',
+    '!**/e2e/**',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/tools/setupTests.ts'],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
+  testPathIgnorePatterns: ['<rootDir>/e2e'],
+};
