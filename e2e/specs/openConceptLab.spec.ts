bf3f4bbc87848722c4dc0a547026eac3448474e0
Dilan Kavishka <dkgamage01@gmail.com>
2023-12-20 15:19:57 +0530

    Add BDD steps on tests

--- e2e/specs/openConceptLab.spec.ts
+++ e2e/specs/openConceptLab.spec.ts
@@ -13,22 +13,26 @@ test.beforeEach(async ({ api }) => {
 test('should be able to setup a subscription and import concepts', async ({ page }) => {
   const openConceptLabPage = new OpenConceptLabPage(page);
 
-  // Setup the subscription
+  await test.step('When the user setup the subscription', async () => {
   await openConceptLabPage.goto();
   await openConceptLabPage.addOclSubscription();
+  });
 
-  // Start an Import
+  await test.step('And the user starts and import', async () => {
   await openConceptLabPage.importTab().click();
   await openConceptLabPage.startImport();
+  });
 
-  // Check results of the import
+  await test.step('Then the import results should show in the test report', async () => {
   await openConceptLabPage.goto();
   await openConceptLabPage.previousImportsTab().click();
   await expect(openConceptLabPage.previousImportsTable()).toHaveText(/\d+ items fetched/);
+  });
 
-  // Unsubscribe
+  await test.step('And the user unsubscribes', async () => {
   await openConceptLabPage.subscriptionTab().click();
   await openConceptLabPage.unsubscribe();
+  });
 });
 
 test.afterEach(async ({ api }) => {
