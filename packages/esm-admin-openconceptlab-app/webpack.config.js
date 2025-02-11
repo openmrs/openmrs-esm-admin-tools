fe918344f1b519bac717073e1a0fd9bbf5dc8d32
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2025-02-11 20:08:53 +0530

    remove webpack config

--- packages/esm-admin-openconceptlab-app/webpack.config.js
+++ packages/esm-admin-openconceptlab-app/webpack.config.js
@@ -1,10 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-const config = (module.exports = require('openmrs/default-webpack-config'));
-config.overrides.resolve = {
-  extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss', '.json'],
-  alias: {
-    '@tools': path.resolve(__dirname, 'tools/'),
-  },
-};
-module.exports = config;
+module.exports = require('openmrs/default-webpack-config');
