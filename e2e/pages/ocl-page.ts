86be6a3585889c5767507667c4438a98af662f77
dilankavishka <dkgamage01@gmail.com>
2025-02-18 15:24:34 +0530

    Remove all unused functions on ocl-page.ts

--- e2e/pages/ocl-page.ts
+++ e2e/pages/ocl-page.ts
@@ -12,13 +12,6 @@ export class OpenConceptLabPage {
     await this.page.goto('ocl');
   }
 
-  async addOclSubscription() {
-    await expect(this.page.getByRole('heading', { name: /ocl subscription module/i })).toBeVisible();
-    await this.page.getByLabel('Subscription URL').fill(process.env.E2E_OCL_SUBSCRIPTION_URL);
-    await this.page.getByLabel('Token').fill(process.env.E2E_OCL_TOKEN);
-    await this.page.getByRole('button', { name: 'Save Changes' }).click();
-  }
-
   async startImport() {
     await this.page.getByRole('button', { name: 'Import from Subscription' }).click();
   }
