68aca866a94fedaf89fe0c936584d074d2853bb0
Ian <ian.c.bacher@gmail.com>
2025-05-01 08:15:05 -0400

    (chore) Update OpenMRS dependencies

--- packages/esm-reports-app/src/declarations.d.ts
+++ packages/esm-reports-app/src/declarations.d.ts
@@ -5,15 +5,3 @@ declare module '*.png';
 declare type SideNavProps = {
   isChildOfHeader: boolean;
 };
-declare module 'react-aria' {
-  export const I18nProvider: (...args: any) => JSX.Element;
-  export { DateValue } from '@react-types/datepicker';
-  export const mergeProps: any;
-  export const useLocale: any;
-  export const useDateField: any;
-  export const useDatePicker: any;
-  export const useDateSegment: any;
-  export const useFocusRing: any;
-  export const useHover: any;
-  export const useObjectRef: any;
-}
