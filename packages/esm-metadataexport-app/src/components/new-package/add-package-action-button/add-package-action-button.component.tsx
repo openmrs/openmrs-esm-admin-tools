9d0f875bf7f2b9c499da383ebbfc4213d9d15ad0
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-08-21 08:40:52 +0530

    add permission check for managing new package workspaces

--- packages/esm-metadataexport-app/src/components/new-package/add-package-action-button/add-package-action-button.component.tsx
+++ packages/esm-metadataexport-app/src/components/new-package/add-package-action-button/add-package-action-button.component.tsx
@@ -3,13 +3,21 @@ import { useTranslation } from 'react-i18next';
 import { Button } from '@carbon/react';
 import { Add } from '@carbon/react/icons';
 import { launchAddNewPackageWorkspace } from '../new-package-utills';
+import { useSession } from '@openmrs/esm-framework';
 
 const NewPackageActionButton: React.FC = () => {
   const { t } = useTranslation();
+  const session = useSession();
+
+  const canManage = session?.user?.privileges?.some((p) => p.display === 'Manage Metadata Export Packages');
 
   const handleAddNewPackageWorkspace = useCallback(() => {
     launchAddNewPackageWorkspace(t);
   }, [t]);
+
+  if (!canManage) {
+    return null;
+  }
 
   return (
     <Button
