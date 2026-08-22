1082b328a6a4fc658bff87ec0cac426750de3099
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-08-22 08:42:47 +0530

    make exportPackage parameter is required

--- packages/esm-metadataexport-app/src/components/view-package/view-package-utills.tsx
+++ packages/esm-metadataexport-app/src/components/view-package/view-package-utills.tsx
@@ -2,7 +2,7 @@ import { type TFunction } from 'i18next';
 import { launchWorkspace } from '@openmrs/esm-framework';
 import { type ExportPackage } from '../../types/index';
 
-export const launchViewPackageWorkspace = (t: TFunction, exportPackage?: ExportPackage) => {
+export const launchViewPackageWorkspace = (t: TFunction, exportPackage: ExportPackage) => {
   launchWorkspace('view-package-workspace', {
     workspaceTitle: exportPackage.name,
     exportPackage,
