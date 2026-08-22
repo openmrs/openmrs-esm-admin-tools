5d10027c0f6e7b47e0c647a96e8bc9ad92b95d98
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-08-22 08:41:18 +0530

    add view package util

---
+++ packages/esm-metadataexport-app/src/components/view-package/view-package-utills.tsx
@@ -0,0 +1,10 @@
+import { type TFunction } from 'i18next';
+import { launchWorkspace } from '@openmrs/esm-framework';
+import { type ExportPackage } from '../../types/index';
+
+export const launchViewPackageWorkspace = (t: TFunction, exportPackage?: ExportPackage) => {
+  launchWorkspace('view-package-workspace', {
+    workspaceTitle: exportPackage.name,
+    exportPackage,
+  });
+};
