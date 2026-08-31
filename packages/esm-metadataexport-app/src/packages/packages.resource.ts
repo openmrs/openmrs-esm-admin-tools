6b08ccd96a878144018694df4fad35f43ea93b34
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-08-31 18:31:14 +0530

    add triggerBuild and deleteBuild functions for package management

--- packages/esm-metadataexport-app/src/packages/packages.resource.ts
+++ packages/esm-metadataexport-app/src/packages/packages.resource.ts
@@ -44,3 +44,26 @@ export function usePackageBuilds(uuid: String) {
     mutate,
   };
 }
+
+export function triggerBuild(
+  uuid: String,
+  abortController?: AbortController,
+): Promise<FetchResponse<ExportPackageBuild>> {
+  return openmrsFetch<ExportPackageBuild>(`${restBaseUrl}/metadataexport/packages/${uuid}/builds`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    signal: abortController?.signal,
+  });
+}
+
+export function deleteBuild(
+  uuid: String,
+  reason?: string,
+  abortController?: AbortController,
+): Promise<FetchResponse<void>> {
+  return openmrsFetch<void>(`${restBaseUrl}/metadataexport/packages/${uuid}?reason=${reason}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    signal: abortController?.signal,
+  });
+}
