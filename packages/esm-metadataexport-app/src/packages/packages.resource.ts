fccbf9b201f6d99a40ab7d1436d8ed3287b4dfe1
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-09-01 19:17:48 +0530

    import statement formatting in packages.resource.ts

--- packages/esm-metadataexport-app/src/packages/packages.resource.ts
+++ packages/esm-metadataexport-app/src/packages/packages.resource.ts
@@ -1,6 +1,6 @@
 import useSWR from 'swr';
 import { type FetchResponse, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
-import { type ExportPackage, type ExportPackageRequest, type ExportPackageBuild } from '../types';
+import type { ExportPackage, ExportPackageRequest, ExportPackageBuild } from '../types';
 
 export function useAllPackages(includeRetired = false) {
   const apiUrl = `${restBaseUrl}/metadataexport/packages?includeRetired=${includeRetired}`;
