7207b254ba86971c2c14d8714efd50e9db0f3d79
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-09-09 19:02:34 +0530

    Add active build status handling and add refresh interval

--- packages/esm-metadataexport-app/src/packages/packages.resource.ts
+++ packages/esm-metadataexport-app/src/packages/packages.resource.ts
@@ -1,6 +1,6 @@
 import useSWR from 'swr';
 import { type FetchResponse, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
-import type { ExportPackage, ExportPackageRequest, ExportPackageBuild } from '../types';
+import type { ExportBuildStatus, ExportPackage, ExportPackageRequest, ExportPackageBuild } from '../types';
 
 export function useAllPackages(includeRetired = false) {
   const apiUrl = `${restBaseUrl}/metadataexport/packages?includeRetired=${includeRetired}`;
@@ -32,10 +32,22 @@ export function createPackage(
 
 export function usePackageBuilds(uuid: string) {
   const apiUrl = `${restBaseUrl}/metadataexport/packages/${uuid}/builds`;
+  const activeBuildStatuses: Array<ExportBuildStatus> = ['QUEUED', 'RUNNING'];
+
   const { data, error, isLoading, isValidating, mutate } = useSWR<FetchResponse<Array<ExportPackageBuild>>, Error>(
     apiUrl,
     openmrsFetch,
+    {
+      refreshInterval: (data) => {
+        const builds = data?.data ?? [];
+
+        const hasActiveBuild = builds.some((build) => activeBuildStatuses.includes(build.status));
+
+        return hasActiveBuild ? 2000 : 0;
+      },
+    },
   );
+
   return {
     builds: data?.data ?? [],
     isLoading,
