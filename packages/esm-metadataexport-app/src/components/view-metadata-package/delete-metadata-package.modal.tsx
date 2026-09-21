47c4f20609fdbac561b66be85b9d886239c674eb
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-09-21 21:25:38 +0530

    update import path for delete metadata package styles

--- packages/esm-metadataexport-app/src/components/view-metadata-package/delete-metadata-package.modal.tsx
+++ packages/esm-metadataexport-app/src/components/view-metadata-package/delete-metadata-package.modal.tsx
@@ -5,7 +5,7 @@ import { useSWRConfig } from 'swr';
 import { OpenmrsFetchError, restBaseUrl, showSnackbar } from '@openmrs/esm-framework';
 import { deletePackage } from '../../packages/packages.resource';
 import type { ExportPackage } from '../../types';
-import styles from './delete-package.modal.scss';
+import styles from './delete-metadata-package.modal.scss';
 
 const packagesUrl = `${restBaseUrl}/metadataexport/packages`;
 const isPackagesCacheKey = (key: unknown) => typeof key === 'string' && key.startsWith(packagesUrl);
