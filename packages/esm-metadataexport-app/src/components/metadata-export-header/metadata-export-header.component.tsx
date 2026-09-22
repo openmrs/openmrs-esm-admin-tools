184b90b9c0e4ae3439fa6a54052c684c18b23d0d
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-09-22 23:04:40 +0530

    remove unused styles

--- packages/esm-metadataexport-app/src/components/metadata-export-header/metadata-export-header.component.tsx
+++ packages/esm-metadataexport-app/src/components/metadata-export-header/metadata-export-header.component.tsx
@@ -6,14 +6,7 @@ import AddPackageActionButton from '../metadata-package-form/add-package-action-
 export const MetaDataExportHeader: React.FC<{ title: string }> = ({ title }) => {
   return (
     <PageHeader className={styles.header} data-testid="metadata-export-header">
-      <PageHeaderContent
-        illustration={
-          <>
-            <MetadataExportPictogram className={styles.metadataExportPictogram} />
-          </>
-        }
-        title={title}
-      />
+      <PageHeaderContent illustration={<MetadataExportPictogram size={72} />} title={title} />
 
       <div className={styles['right-justified-items']}>
         <AddPackageActionButton />
